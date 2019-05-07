fe2ae54271fd17e4d4467a2c076ab2eacc6a174b
tkbacher <tiffanybacher@gmail.com>
2019-05-07 15:23:03 -0600

    Add movies to store
    Co-authored-by: jacobogart <jacobogart@gmail.com>

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -4,19 +4,29 @@ import { Switch, Route } from 'react-router';
 import './App.css';
 import { fetchDiscover } from '../../api/fetchDiscover';
 import { cleanDiscover } from '../../api/cleaners';
+import { addDiscoverMovies } from '../../actions'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      error: ''
+    }
+  }
   
   componentDidMount() {
-    fetchDiscover()
+    return fetchDiscover()
       .then(response => cleanDiscover(response.results))
-      .then(movies => console.log(movies));
+      .then(movies => this.props.addDiscoverMovies(movies))
+      .catch(error => this.setState({error: error.message}));
   }
   
   render() {
+    console.log(this.props.movies)
     return(
       <div className="App">
         <h1>OUR APP</h1>
+        {this.props.movies.map(movie => <h3>{movie.title}</h3>)}
         {/* <Header />
         <Switch>
           <Route exact path='/' component={Splash}/>
@@ -27,4 +37,12 @@ class App extends Component {
   }
 }
 
-export default connect()(App);
+const mapStateToProps = (state) => ({
+  movies: state.movies
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  addDiscoverMovies: (movies) => dispatch(addDiscoverMovies(movies))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
