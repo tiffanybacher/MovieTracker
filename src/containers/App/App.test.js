fce5bc033ac24b461bde874d22917fc9f82a70af
jacobogart <jacobogart@gmail.com>
2019-05-07 18:42:19 -0600

    Set up eznyme dependencies

--- src/containers/App/App.test.js
+++ src/containers/App/App.test.js
@@ -1,4 +1,14 @@
 import React from 'react';
+import { shallow } from 
 import App from './App';
 
+describe('App', () => {
+  let wrapper;
 
+  beforeEach(() => {
+    wrapper = shallow(<App />)
+  })
+  it('should match the snapshot', () => {
+
+  })
+})
