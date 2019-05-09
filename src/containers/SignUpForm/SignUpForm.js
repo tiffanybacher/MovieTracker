a3952e50148792a3121ac1e68478b76fe16c3d69
jacobogart <jacobogart@gmail.com>
2019-05-09 13:24:13 -0600

    Add error handling for creating new account

--- src/containers/SignUpForm/SignUpForm.js
+++ src/containers/SignUpForm/SignUpForm.js
@@ -3,13 +3,15 @@ import { Link } from 'react-router-dom';
 import { fetchNewUser } from '../../api/fetchNewUser';
 import { connect } from 'react-redux';
 import { updateUser } from '../../actions';
+import { fetchAllUsers } from '../../api/fetchAllUsers';
 
 export class SignUpForm extends Component {
   state = {
     email: '',
     name: '',
     password: '',
-    passwordConfirm: ''
+    passwordConfirm: '',
+    error: '',
   }
 
   handleChange = (e) => {
@@ -21,58 +23,93 @@ export class SignUpForm extends Component {
   }
 
   handleSubmit = (e) => {
-    const { email, name, password } = this.state;
-
     e.preventDefault();
+    const { email, name, password, passwordConfirm } = this.state;
+    this.setState( { error: ''} )
+    fetchAllUsers()
+      .then(data => {
+        this.checkEmail(data, email);
+        this.checkPasswords(password, passwordConfirm);
+        if (!this.state.error) {
+          fetchNewUser(email, name, password)
+            .then(id => {
+              this.props.updateUser(id, name);
+              this.props.history.push("/");
+            })
+            .catch(error => console.log(error));
+        }
+      })
+      .catch(error => console.log(error));
+  }
 
-    fetchNewUser(email, name, password)
-      .then(id => this.props.updateUser(id, name));
+  checkEmail = (data, email) => {
+    data.forEach(user => {
+      if(user.email === email) {
+        this.setState({ error: 'Email is already taken. Please try again.' })
+      }
+    });
+  }
+
+  checkPasswords = (password, passwordConfirm) => {
+    if(password !== passwordConfirm) {
+      this.setState({ error: 'Passwords do not match. Please try again.' })
+    }
   }
 
   render() {
+    let { error } = this.state;
     return (
       <div className="SignUpForm">
         <h2>Sign up for an account</h2>
-        <p>Sign up is free and easy! Fill out the form below to get started.</p>
-        <form
-          onSubmit={this.handleSubmit}
-          className="signup-form-inputs"
-        >
+        <p>
+          Sign up is free and easy! Fill out the form below to get started.
+        </p>
+        <form onSubmit={this.handleSubmit} className="signup-form-inputs">
           <label htmlFor="signup-email-input">Email</label>
-          <input 
-            type="text" 
+          <input
+            type="text"
             id="signup-email-input"
             name="email"
-            value={this.state.email} 
+            value={this.state.email}
             onChange={this.handleChange}
           />
+          {error === "Email is already taken. Please try again." && (
+            <p className="error-message">{error}</p>
+          )}
           <label htmlFor="signup-name-input">Name</label>
-          <input 
-            type="text" 
+          <input
+            type="text"
             id="signup-name-input"
             name="name"
-            value={this.state.name} 
+            value={this.state.name}
             onChange={this.handleChange}
           />
           <label htmlFor="signup-password-input">Password</label>
-          <input 
-            type="password" 
+          <input
+            type="password"
             id="signup-password-input"
-            name="password" 
+            name="password"
             value={this.state.password}
             onChange={this.handleChange}
           />
           <label htmlFor="password-confirm-input">Confirm Password</label>
-          <input 
-            type="password" 
+          <input
+            type="password"
             id="password-confirm-input"
-            name="passwordConfirm" 
+            name="passwordConfirm"
             value={this.state.passwordConfirm}
             onChange={this.handleChange}
           />
+          {error === 'Passwords do not match. Please try again.' && (
+            <p className="error-message">{error}</p>
+          )}
           <div className="signup-btn-wrapper">
-            <button type="submit" className="signup-btn btn">Sign Up</button>
-            <Link to="/" className="cancel-link">Cancel</Link>
+            <button type="submit" className="signup-btn btn">
+              Sign Up
+            </button>
+            <Link to="/" className="cancel-link">
+              Cancel
+            </Link>
           </div>
         </form>
         <article className="signup-blurb">
