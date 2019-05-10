99ded511abc390ee89a372d73482fb6fad7f32d8
tkbacher <tiffanybacher@gmail.com>
2019-05-09 21:47:43 -0600

    Add search icon to search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,6 +4,7 @@ class SearchBar extends Component {
   render() {
     return (
       <div className="SearchBar">
+        <i class="fas fa-search"></i>
         <input type="text" className="search-input" placeholder="Search for a movie..."/>
       </div>
     );
