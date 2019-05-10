5e2747c23483cda878f1729a52988a1c6b7a81bb
tkbacher <tiffanybacher@gmail.com>
2019-05-09 20:43:27 -0600

    Change header logo and add nav hover states

--- src/containers/Nav/Nav.js
+++ src/containers/Nav/Nav.js
@@ -60,7 +60,9 @@ class Nav extends Component {
     return (
       <nav className="Nav">
         <div className="nav-left">
-          <img src="https://www.themoviedb.org/assets/2/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png" className="header-logo" alt="Logo" />
+        <Link to="/" className="main-header"><h1>MOVIE TRACKER</h1></Link>
+          {//<img src="https://www.themoviedb.org/assets/2/v4/logos/408x161-powered-by-rectangle-green-bb4301c10ddc749b4e79463811a68afebeae66ef43d17bcfd8ff0e60ded7ce99.png" className="header-logo" alt="Logo" />
+          }
           <div className="main-nav-wrapper">
             <NavLink to="/" className="nav-link">
               EXPLORE
