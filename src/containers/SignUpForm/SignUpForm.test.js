78bbb7ccc73db4078fdbf4b3fb29467baec7546d
jacobogart <jacobogart@gmail.com>
2019-05-13 12:58:45 -0500

    Add SignUpForm component tests

--- src/containers/SignUpForm/SignUpForm.test.js
+++ src/containers/SignUpForm/SignUpForm.test.js
@@ -2,9 +2,12 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import { SignUpForm, mapDispatchToProps } from './SignUpForm';
 import { fetchNewUser } from '../../api/fetchNewUser';
+import { fetchAllUsers } from '../../api/fetchAllUsers';
 import { updateUser } from '../../actions';
+import { mockUser } from '../../api/__tests__/mockData';
 
 jest.mock('../../api/fetchNewUser');
+jest.mock('../../api/fetchAllUsers')
 
 const mockChangeEvent = {
   target: {
@@ -16,19 +19,35 @@ const mockSubmitEvent = {
   preventDefault: () => {}
 };
 
+const mockPasswords = {
+  password: "A",
+  passwordConfirm: "B"
+};
+
 describe('SignUpForm', () => {
   let wrapper, instance;
   let mockUpdateUser = jest.fn();
+  let mockHistory = {
+    push: jest.fn()
+  }
   
   fetchNewUser.mockImplementation(() => Promise.resolve(1));
+  fetchAllUsers.mockImplementation(() => Promise.resolve({
+    data: [mockUser, mockUser]
+  }));
 
   beforeEach(() => {
-    wrapper = shallow(<SignUpForm updateUser={mockUpdateUser} />);
+    wrapper = shallow(
+      <SignUpForm 
+        updateUser={mockUpdateUser} 
+        history={mockHistory}
+      />);
     instance = wrapper.instance();
   });
 
   afterEach(() => {
     fetchNewUser.mockClear();
+    fetchAllUsers.mockClear();
   });
 
   it('should match the snapshot', () => {
@@ -40,7 +59,8 @@ describe('SignUpForm', () => {
       email: '',
       name: '',
       password: '',
-      passwordConfirm: ''
+      passwordConfirm: '',
+      error: ' '
     };
 
     wrapper = shallow(<SignUpForm />, { disableLifecycleMethods: true });
@@ -55,7 +75,7 @@ describe('SignUpForm', () => {
 
     it('should invoke handleSubmit on form submit', () => {
       wrapper.find(".signup-form-inputs").simulate('submit', mockSubmitEvent);
-      expect(fetchNewUser).toHaveBeenCalled();
+      expect(fetchAllUsers).toHaveBeenCalled();
     });
   });
 
@@ -67,14 +87,86 @@ describe('SignUpForm', () => {
   });
 
   describe("handleSubmit", () => {
-    it("should invoke fetchNewUser", () => {
+    it('should set error in state to a single space', () => {
+      instance.setState({ error: 'test error' });
+      expect(wrapper.state('error')).toEqual('test error');
+
       instance.handleSubmit(mockSubmitEvent);
+      expect(wrapper.state('error')).toEqual(' ');
+    });
+
+    it("should invoke fetchAllUsers", () => {
+      instance.handleSubmit(mockSubmitEvent);
+      expect(fetchAllUsers).toHaveBeenCalled();
+    });
+
+    it('should invoke checkEmail method after fetchAllUsers resolves', async () => {
+      jest.spyOn(instance, 'checkEmail');
+      await instance.handleSubmit(mockSubmitEvent);
+      expect(instance.checkEmail).toHaveBeenCalled();
+    });
+
+    it('should invoke checkPasswords method after fetchAllUsers resolves', async () => {
+      instance.checkEmail = jest.fn();
+      jest.spyOn(instance, 'checkPasswords');
+      await instance.handleSubmit(mockSubmitEvent);
+      expect(instance.checkPasswords).toHaveBeenCalled();
+    });
+
+    it('should invoke fetchNewUser if there are no errors in state', async () => {
+      instance.checkEmail = jest.fn();
+      instance.checkPasswords = jest.fn();
+      await instance.handleSubmit(mockSubmitEvent);
       expect(fetchNewUser).toHaveBeenCalled();
     });
 
-    it("should invoke updateUser", () => {
-      instance.handleSubmit(mockSubmitEvent);
+    it("should not invoke fetchNewUser if there are errors in state", async () => {
+      instance.setState(mockPasswords);
+      instance.checkEmail = jest.fn();
+      await instance.handleSubmit(mockSubmitEvent);
+      expect(fetchNewUser).not.toHaveBeenCalled();
+    });
+
+    it("should invoke updateUser", async () => {
+      await instance.handleSubmit(mockSubmitEvent);
       expect(mockUpdateUser).toHaveBeenCalled();
+    });
+
+    it("should invoke the push method to change the route", async () => {
+      await instance.handleSubmit(mockSubmitEvent);
+      expect(mockHistory.push).toHaveBeenCalledWith('/');
+    });
+  });
+
+  describe('checkEmail', () => {
+    const mockUserData = [ { email: 'email@email.com' }, {email: 'test@test.com'}];
+
+    it('should set an error in state if the given email matches any existing emails', () => {
+      expect(wrapper.state('error')).toEqual(' ');
+      instance.checkEmail(mockUserData, 'email@email.com');
+      expect(wrapper.state('error')).toEqual('Email is already taken. Please try again.');
+    });
+
+    it('should set not an error in state if the given email is new', () => {
+      expect(wrapper.state('error')).toEqual(' ');
+      instance.checkEmail(mockUserData, 'new@new.com');
+      expect(wrapper.state('error')).toEqual(' ');
+    });
+  });
+
+  describe("checkPasswords", () => {
+    it("should set an error in state if the passwords don't match", () => {
+      expect(wrapper.state("error")).toEqual(" ");
+      instance.checkPasswords('A', 'B');
+      expect(wrapper.state("error")).toEqual(
+        "Passwords do not match. Please try again."
+      );
+    });
+
+    it("should set not an error in state if the given passwords match", () => {
+      expect(wrapper.state("error")).toEqual(" ");
+      instance.checkPasswords('A', 'A');
+      expect(wrapper.state("error")).toEqual(" ");
     });
   });
 
